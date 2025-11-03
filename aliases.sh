df9cdf2175dee389bf50033b3d2159809058e63f
StafLoker <151257985+StafLoker@users.noreply.github.com>
2025-11-03 13:56:11 +0100

    Add common shell aliases for convenience

---
+++ aliases.sh
@@ -0,0 +1,30 @@
+alias ll='ls -lah'
+alias llg='ls -lah | grep -i'
+
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+alias h='history'
+alias hg='history | grep'
+
+alias cmx='chmod +x'
+
+alias f='find . -iname'
+
+alias td='tree -d'
+alias tdl='tree -d -L 4'
+
+alias s='systemctl'
+alias srt='systemctl start'
+alias sp='systemctl stop'
+alias sst='systemctl status'
+alias sr='systemctl restart'
+alias sdr='systemctl daemon-reload'
+
+alias j='journalctl -xeu'
+alias jctl='journalctl -u'
+alias jrt='journalctl -fu'
+
+alias d='docker'
+alias dc='docker compose'
